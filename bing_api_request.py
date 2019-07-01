30c69a7ae6f84f1c924e1863ab48ecaeb998b194
mgtDHsuRA <mgtDHsuRA@wharton.upenn.edu>
2019-07-01 14:50:03 -0400

    updated and renamed files to remove dashes

---
+++ bing_api_request.py
@@ -0,0 +1,127 @@
+import requests
+import csv
+from unidecode import unidecode
+
+with open('files/affiliationnamefreq100.tsv', encoding='latin-1') as tsvfile:
+    with open('files/bing_output_2.0.tsv', 'w', newline="\n", encoding='utf-8') as out_file:
+        
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        tsv_writer.writerow(['affiliation', 'freq', 'formatted_address', 'locality', 'admin_district', 'country', 'lat', 'lng'])
+        reader = csv.DictReader(tsvfile, dialect='excel-tab')
+        cnt = 0
+        
+        for row in reader:
+            cnt += 1
+            
+            #only print out every 100th number
+            if cnt % 100 == 0:
+                print(cnt)
+            
+            input_name = unidecode(str(row['affiliationname']))
+            freq = row['freq']
+            
+            #bing
+            response = requests.get("http://dev.virtualearth.net/REST/v1/Locations/?",
+                        params={"query": input_name,
+                                "include": "queryParse",
+                                "key": "api_key"})
+            
+            try:
+                response.json()
+            except ValueError:
+                tsv_writer.writerow([input_name, freq, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A'])
+                continue
+           
+            try:
+                data = response.json()
+                
+                row_list = []
+                row_list.append(input_name)
+                row_list.append(freq)
+                
+                # formatted_address
+                try:
+                    #bing
+                    address = data['resourceSets'][0]['resources'][0]['address']['formattedAddress']
+                    row_list.append(address)
+                except:
+                    row_list.append("N/A")
+                
+                # locality
+                try:
+                    #bing
+                    locality = data['resourceSets'][0]['resources'][0]['address']['locality']
+                    row_list.append(locality)
+                except:
+                    row_list.append("N/A")
+                
+                #admin district or state
+                try:
+                    #bing
+                    admin_district = data['resourceSets'][0]['resources'][0]['address']['adminDistrict']
+                    row_list.append(admin_district)
+                except:
+                    row_list.append("N/A")
+                    
+                # country
+                try:
+                    #bing
+                    country = data['resourceSets'][0]['resources'][0]['address']['countryRegion']
+                    row_list.append(country)
+                except:
+                    row_list.append("N/A")
+                
+                
+                # get lat and long
+                try:
+                    #bing
+                    lat = data['resourceSets'][0]['resources'][0]['point']['coordinates'][0]
+                    lng = data['resourceSets'][0]['resources'][0]['point']['coordinates'][1]
+                    row_list.append(lat)
+                    row_list.append(lng)
+                except:
+                    row_list.append("N/A")
+                    row_list.append("N/A")
+                    
+                tsv_writer.writerow(row_list)
+                
+            except:
+                data = response.json()
+            
+                row_list = []
+                row_list.append(input_name)
+                row_list.append(freq)
+                
+                # formatted_address
+                address = data['resourceSets'][0]['resources'][0]['address']['formattedAddress']
+                row_list.append(unidecode(address))
+                
+                
+                
+                # locality
+                locality = data['resourceSets'][0]['resources'][0]['address']['locality']
+                row_list.append(unidecode(locality))
+                
+                #admin district or state
+                admin_district = data['resourceSets'][0]['resources'][0]['address']['adminDistrict']
+                row_list.append(unidecode(admin_district))
+                
+                # country
+                country = data['resourceSets'][0]['resources'][0]['address']['countryRegion']
+                row_list.append(unidecode(country))
+                
+                
+                # get lat and long
+                try:
+                    #bing
+                    lat = data['resourceSets'][0]['resources'][0]['point']['coordinates'][0]
+                    lng = data['resourceSets'][0]['resources'][0]['point']['coordinates'][1]
+                    row_list.append(lat)
+                    row_list.append(lng)
+                except:
+                    row_list.append("N/A")
+                    row_list.append("N/A")
+                    
+                tsv_writer.writerow(row_list)
+
+                
---
+++ google_api_request.py
@@ -0,0 +1,139 @@
+import requests
+import csv
+from unidecode import unidecode
+
+
+with open('files/affiliationnamefreq100.tsv', encoding='latin-1') as tsvfile:
+    with open('files/google_output.tsv', 'w', newline="\n", encoding='utf-8') as out_file:
+        
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        tsv_writer.writerow(['affiliation', 'freq', "formatted_address",'locality', 'admin_district', 'country', 'lat', 'lng'])
+        reader = csv.DictReader(tsvfile, dialect='excel-tab')
+        cnt = 0
+        
+        for row in reader:
+            cnt += 1
+            
+            #only print out every 100th number
+            if cnt % 100 == 0:
+                print(cnt)
+                
+            row_information = []
+            
+            input_name = unidecode(str(row['affiliationname']))
+            row_information.append(input_name)
+            
+            freq = row['freq']
+            row_information.append(freq)
+            
+            #google
+            response = requests.get("https://maps.googleapis.com/maps/api/geocode/json", 
+                    params={"key": "api_key", 
+                            "address": input_name})
+            
+            try:
+                response.json()
+            except ValueError:
+                tsv_writer.writerow([input_name, freq, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A'])
+                continue
+            
+            try:
+                data = response.json()
+            
+                #get the formatted address
+                try:
+                    formatted_address = data['results'][0]['formatted_address']
+                    row_information.append(formatted_address)
+                except:
+                    row_information.append("N/A")
+                    
+                #get locality
+                try:
+                    for component in data['results'][0]['address_components']:
+                        if 'locality' in component['types'] or 'sublocality' in component['types']:
+                            locality = component['long_name']
+                    row_information.append(locality)
+                except:
+                    row_information.append("N/A")
+                    
+                #get the admin district
+                try:
+                    for component in data['results'][0]['address_components']:
+                        if 'administrative_area_level_1' in component['types']:
+                            admin_district = component['long_name']
+                    row_information.append(admin_district)
+                except:
+                    row_information.append("N/A")
+                            
+                #get the country
+                try:
+                    for component in data['results'][0]['address_components']:
+                        if 'country' in component['types']:
+                            country = component['long_name']
+                    row_information.append(country)
+                except:        
+                    row_information.append("N/A")
+                    
+                try:
+                    lat = data['results'][0]['geometry']['location']['lat']
+                    lng = data['results'][0]['geometry']['location']['lng']
+                except:
+                    lat = "N/A"
+                    lng = "N/A"
+                    
+                row_information.append(lat)
+                row_information.append(lng)
+                
+                tsv_writer.writerow(row_information)
+            
+            except:
+                data = response.json()
+                row_information = []
+                row_information.append(input_name)
+                row_information.append(freq)
+            
+                #get the formatted address
+                try:
+                    formatted_address = data['results'][0]['formatted_address']
+                    row_information.append(unidecode(formatted_address))
+                except:
+                    row_information.append("N/A")
+                    
+                #get locality
+                try:
+                    for component in data['results'][0]['address_components']:
+                        if 'locality' in component['types'] or 'sublocality' in component['types']:
+                            locality = component['long_name']
+                    row_information.append(unidecode(locality))
+                except:
+                    row_information.append("N/A")
+                    
+                #get the admin district
+                try:
+                    for component in data['results'][0]['address_components']:
+                        if 'administrative_area_level_1' in component['types']:
+                            admin_district = component['long_name']
+                    row_information.append(unidecode(admin_district))
+                except:
+                    row_information.append("N/A")
+                            
+                #get the country
+                try:
+                    for component in data['results'][0]['address_components']:
+                        if 'country' in component['types']:
+                            country = component['long_name']
+                    row_information.append(unidecode(country))
+                except:        
+                    row_information.append("N/A")
+                    
+                try:
+                    lat = data['results'][0]['geometry']['location']['lat']
+                    lng = data['results'][0]['geometry']['location']['lng']
+                except:
+                    lat = "N/A"
+                    lng = "N/A"
+                    
+                row_information.append(lat)
+                row_information.append(lng)
+                
+                tsv_writer.writerow(row_information)
